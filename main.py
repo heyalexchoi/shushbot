a22495073042e9088b15ad3368f140bf8d629fef
alex choi <heyalexchoi@gmail.com>
2018-07-18 20:42:58 -0400

    tuned thresholds

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ from audio import play, listen
 def too_loud_callback(rms):
     print(f"too_loud_callback rms: {rms}")
 
-    if rms > 600:
+    if rms > 2000:
         play.play_arnold_shut_up(volume=0)
-        print('------arnold')
-    elif rms > 400:
+        print('-------------------------------------------------------------------------1200')
+    elif rms > 1200:
         play.play_shush(volume=0)
-        print('------400')
-    elif rms > 200:
+        print('-------------------------------------------------------------------------900')
+    elif rms > 600:
         play.play_shush(volume=-600)
-        print('------200')
-    elif rms > 100:
+        print('-------------------------------------------------------------------------600')
+    elif rms > 300:
         play.play_shush(volume=-2000)
-        print('------100')
+        print('-------------------------------------------------------------------------300')
 
 print('shushbot listening to shush')
 listen.listen(threshold=0, too_loud_callback=too_loud_callback)
